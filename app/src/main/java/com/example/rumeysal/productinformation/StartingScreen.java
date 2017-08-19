268257aaecfbc8e3656f46b9d261181b4a57cff1
Rumeysa, Bingöl <rumeysa.bingol@agu.edu.tr>
2017-08-19 11:31:06 +0300

    merged

--- app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
+++ app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
@@ -1,6 +1,7 @@
 package com.example.rumeysal.productinformation;
 
 import android.content.Intent;
+<<<<<<< HEAD
 
 import android.os.AsyncTask;
 import android.support.annotation.IdRes;
@@ -18,16 +19,33 @@ public class StartingScreen extends AppCompatActivity {
 
     CircularProgressButton Button;
 
+=======
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartingScreen extends AppCompatActivity {
+
+>>>>>>> parent of 2e1493e... start screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
     }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 2e1493e... start screen
     public void Cilis(View view) {
         Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
 
+<<<<<<< HEAD
         startActivity(intent);
     }
+=======
+        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
+        startActivity(intent);}
+
+>>>>>>> parent of 2e1493e... start screen
 }
