7cd27c3e3627b3edff00b1e724f4743793ec3570
aec2 <aenesc1@gmail.com>
2017-08-19 12:21:02 +0300

    yenilendi

--- app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
+++ app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
@@ -1,51 +1,55 @@
 package com.example.rumeysal.productinformation;
 
 import android.content.Intent;
-<<<<<<< HEAD
-
 import android.os.AsyncTask;
 import android.support.annotation.IdRes;
-import android.support.v7.app.ActionBar;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
 public class StartingScreen extends AppCompatActivity {
 
-
-
     CircularProgressButton Button;
 
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
-public class StartingScreen extends AppCompatActivity {
-
->>>>>>> parent of 2e1493e... start screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        Button = (CircularProgressButton)findViewById(R.id.Baslat);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncTask<String, String, String> demoLogin = new AsyncTask<String, String, String>() {
+                    @Override
+                    protected String doInBackground(String... params) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return "done";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        if(s.equals("done")){
+                            Intent i = new Intent(StartingScreen.this, GirisEkrani.class);
+                            startActivity(i);
+                        }
+                    }
+                };
+
+                Button.startAnimation();
+                demoLogin.execute();
+
+            }
+        });
+
     }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 2e1493e... start screen
-    public void Cilis(View view) {
-        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
-
-<<<<<<< HEAD
-        startActivity(intent);
-    }
-=======
-        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
-        startActivity(intent);}
-
->>>>>>> parent of 2e1493e... start screen
 }
+
+ 
