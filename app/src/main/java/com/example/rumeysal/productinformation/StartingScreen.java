9c1597be8f95227e2836eef988c33c02a5aa20bf
Rumeysa, Bingöl <rumeysa.bingol@agu.edu.tr>
2017-08-19 11:52:05 +0300

    yeni

--- app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
+++ app/src/main/java/com/example/rumeysal/productinformation/StartingScreen.java
@@ -1,10 +1,6 @@
 package com.example.rumeysal.productinformation;
 
 import android.content.Intent;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import android.os.AsyncTask;
 import android.support.annotation.IdRes;
@@ -14,83 +10,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
+//import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
 public class StartingScreen extends AppCompatActivity {
 
 
 
-    CircularProgressButton Button;
+   // CircularProgressButton Button;
 
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
-public class StartingScreen extends AppCompatActivity {
 
->>>>>>> parent of 2e1493e... start screen
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
-public class StartingScreen extends AppCompatActivity {
 
->>>>>>> parent of 2e1493e... start screen
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
-public class StartingScreen extends AppCompatActivity {
-
->>>>>>> parent of d13a930... Merge branch 'master' of https://github.com/rmysbngl/ProductInformation
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
-public class StartingScreen extends AppCompatActivity {
-
->>>>>>> parent of 2e1493e... start screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 2e1493e... start screen
-=======
->>>>>>> parent of 2e1493e... start screen
-=======
->>>>>>> parent of d13a930... Merge branch 'master' of https://github.com/rmysbngl/ProductInformation
-=======
->>>>>>> parent of 2e1493e... start screen
+
     public void Cilis(View view) {
-        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
         startActivity(intent);
-    }
-=======
-=======
->>>>>>> parent of 2e1493e... start screen
-=======
->>>>>>> parent of d13a930... Merge branch 'master' of https://github.com/rmysbngl/ProductInformation
-=======
->>>>>>> parent of 2e1493e... start screen
-        Intent intent=new Intent(StartingScreen.this,GirisEkrani.class);
-        startActivity(intent);}
+}
 
->>>>>>> parent of 2e1493e... start screen
+
 }
