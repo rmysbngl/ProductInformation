46ef141128dca4faf3cecfe7031459380de4a1e5
eliftultay <eliftultay@gmail.com>
2017-09-14 23:38:31 +0300

    Time View added, Some of UI parts changed

--- app/src/main/java/com/example/rumeysal/productinformation/ProgressPart.java
+++ app/src/main/java/com/example/rumeysal/productinformation/ProgressPart.java
@@ -34,63 +34,49 @@ import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 
 public class ProgressPart extends AppCompatActivity  {
     BluetoothSPP bt;
-    static float value;
-    int checkValue=1;
     ProgressBar timer;
     MyCountDownTimer CDT;
-    static int minuteProgress;          //Arduino Giriş sayfasından gelen süre değeri
-    int minute;
-    TextView değer;
+
+    static int minuteProgress;
+    int minute;     //Arduino Giriş sayfasından gelen süre değeri
+    TextView değerV;
+    TextView değerP;
+    TextView değerG;
+
+    static float vacuumValue;
+    static float plasmaValue;
+    static float gasValue;
+
+    int checkValue=1;
+
     private LineChart mChart;
+    private LineChart chartPlasma;
+    private LineChart chartGas;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_part);
+
         timer=(ProgressBar) findViewById(R.id.progressBar) ;            //Progress bar ın kurulumu
         timer.setProgress(100);
         minute=minuteProgress;
         bt=(BTConnect.getBt());
-        değer=(TextView) findViewById(R.id.Values) ;
-        value=0;
+
+        değerV = (TextView) findViewById(R.id.Values);
+/*      değerP = (TextView) findViewById(R.id.txtPlasmaValue);
+        değerG = (TextView) findViewById(R.id.txtGasValue);*/
+
+        vacuumValue = 0;
+/*        plasmaValue = 0;
+        gasValue = 0;*/
 
         //TODO: Tüpün içindeki değer her zaman sabitse bu değeri ona göre kur
 
-       //Grafiğin kurulması
-        mChart=(LineChart) findViewById(R.id.graphVacuum);
-        mChart.setDescription("");
-        mChart.setNoDataText("No data");
-        mChart.setHighlightEnabled(true);
-        mChart.setTouchEnabled(true);
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setDrawGridBackground(false);
-        mChart.setPinchZoom(true);
-        mChart.setBackgroundColor(Color.parseColor("#F9FBE7"));
-
-        LineData data=new LineData();
-        data.setValueTextColor(Color.parseColor("#006064"));
-        mChart.setData(data);
-
-        Legend l =mChart.getLegend();
-        l.setForm(Legend.LegendForm.LINE);
-        l.setTextColor(Color.parseColor("#006064"));
-
-        XAxis Xl=mChart.getXAxis();
-        Xl.setTextColor(Color.parseColor("#006064"));
-        Xl.setDrawGridLines(false);
-        Xl.setAvoidFirstLastClipping(false);
-
-        YAxis Yl=mChart.getAxisLeft();
-        Yl.setDrawGridLines(false);
-        Yl.setTextColor(Color.parseColor("#006064"));
-        Yl.setStartAtZero(false);
-        Yl.setAxisMaxValue(0f);
-        Yl.setAxisMinValue(-100f);
-        YAxis Yaxis=mChart.getAxisRight();
-        Yaxis.setEnabled(false);
-
+        drawVacuumGraph();
+/*        drawPlasmaGraph();
+        drawGasGraph();*/
 
         //Bluetooth üzerinden veri almak için
 
@@ -100,8 +86,9 @@ public class ProgressPart extends AppCompatActivity  {
                String[]veri= message.split(":");
                 //TODO: Eğer vacuum değeri alınıyorsa, alınan verinin formu  (BT: "vacuum değeri" ) şeklinde olmalıdır. iki nokta ve BT kısmı önemli
                 if(veri[0].equals("BT")){
-                        value=Float.valueOf(veri[1].trim());        //alınan veri 'value' ya kurulup sonra 'value' plot ediliyor
-
+                        vacuumValue=Float.valueOf(veri[1].trim());        //alınan veri 'value' ya kurulup sonra 'value' plot ediliyor
+                        /*plasmaValue=Float.valueOf(veri[1].trim());*/
+/*                        gasValue=Float.valueOf(veri[1].trim());*/
                 }
 
                 if((message.trim()).equals("finish")){     //TODO: Bittiğini anlamak için 'finish' komutu gönderilmeli
@@ -125,10 +112,10 @@ public class ProgressPart extends AppCompatActivity  {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if(value!=checkValue){  //Grafiğe yeni bir değerin girdiğini farketmesi için yapıldı. Diğer türlü aynı veriden 3 kere giriliyordu
+                            if(vacuumValue!=checkValue){  //Grafiğe yeni bir değerin girdiğini farketmesi için yapıldı. Diğer türlü aynı veriden 3 kere giriliyordu
                                  addEntry();        //Grafiğe veriyi yazdırmak için
-                                 değer.setText(String.valueOf(value));           //ekranda yazılan veriyi göstermek için
-                                 value=checkValue;
+                                 değerV.setText(String.valueOf(vacuumValue));           //ekranda yazılan veriyi göstermek için
+                                vacuumValue=checkValue;
                             }
 
                         }
@@ -138,7 +125,7 @@ public class ProgressPart extends AppCompatActivity  {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }while (value!=(VacuumNew+1)); //Ayarlanan vakum değerinde grafiğin yazımının bitmesi için //TODO: eğer orda yazandan daha farklı bir değerde çalışma ihtimali varsa cihazdan alınan veriye göre düzenlenmesi lazım
+                }while (vacuumValue!=(VacuumNew+1)); //Ayarlanan vakum değerinde grafiğin yazımının bitmesi için //TODO: eğer orda yazandan daha farklı bir değerde çalışma ihtimali varsa cihazdan alınan veriye göre düzenlenmesi lazım
 
             }
         }).start();
@@ -152,37 +139,103 @@ public class ProgressPart extends AppCompatActivity  {
     }
 
     public void addEntry(){
-        LineData data = mChart.getLineData();
+        LineData vData = mChart.getLineData();
+       /* LineData pData = chartPlasma.getLineData();
+        LineData gData = chartGas.getLineData();*/
 
-        if(data!=null){
-            LineDataSet set =data.getDataSetByIndex(0);
+        if(vData!=null){
+            LineDataSet set = vData.getDataSetByIndex(0);
 
             if(set==null){          //veri yokkende grafiği kurmak için
                 set=createSet();
-                data.addDataSet(set);
+                vData.addDataSet(set);
 
             }
-            data.addXValue("");
-            data.addEntry(new Entry(value,set.getEntryCount()),0); //yeni eklenen verinin grafiğe eklenmesi için
+            vData.addXValue("");
+            vData.addEntry(new Entry(vacuumValue,set.getEntryCount()),0); //yeni eklenen verinin grafiğe eklenmesi için
             mChart.notifyDataSetChanged();
             mChart.setVisibleXRange(200);           //Bütün verilerin aynı grafikte görünmesi için. Eğer veri alındıkça kayması isteniyorsa burdaki sayının  küçüklmesi gerek. Mesela 10'a filan kurulablir.
-            mChart.moveViewToX(data.getXValCount());
+            mChart.moveViewToX(vData.getXValCount());
         }
+
+        /*if(pData!=null){
+            LineDataSet set = pData.getDataSetByIndex(0);
+
+            if(set==null){          //veri yokkende grafiği kurmak için
+                set=plasmaDataSet();
+                pData.addDataSet(set);
+
+            }
+            pData.addXValue("");
+            pData.addEntry(new Entry(value,set.getEntryCount()),0); //yeni eklenen verinin grafiğe eklenmesi için
+            chartPlasma.notifyDataSetChanged();
+            chartPlasma.setVisibleXRange(200);           //Bütün verilerin aynı grafikte görünmesi için. Eğer veri alındıkça kayması isteniyorsa burdaki sayının  küçüklmesi gerek. Mesela 10'a filan kurulablir.
+            chartPlasma.moveViewToX(pData.getXValCount());
+        }
+
+        if(gData!=null){
+            LineDataSet set = gData.getDataSetByIndex(0);
+
+            if(set==null){          //veri yokkende grafiği kurmak için
+                set=gasDataSet();
+                gData.addDataSet(set);
+
+            }
+            gData.addXValue("");
+            gData.addEntry(new Entry(value,set.getEntryCount()),0); //yeni eklenen verinin grafiğe eklenmesi için
+            chartGas.notifyDataSetChanged();
+            chartGas.setVisibleXRange(200);           //Bütün verilerin aynı grafikte görünmesi için. Eğer veri alındıkça kayması isteniyorsa burdaki sayının  küçüklmesi gerek. Mesela 10'a filan kurulablir.
+            chartGas.moveViewToX(gData.getXValCount());
+        }
+*/
     }
+
     private LineDataSet createSet(){
         LineDataSet set=new LineDataSet(null,"Vacuum");
-        set.setColor(Color.BLUE);
+        set.setColor(Color.parseColor("#2196F3"));
         set.setLineWidth(1f);
         set.setCircleSize(1f);
-        set.setCircleColor(Color.BLUE);
-        set.setCircleColorHole(Color.BLUE);
-        set.setFillColor(Color.BLUE);
-        set.setHighLightColor(Color.BLUE);
-        set.setValueTextColor(Color.WHITE);
+        set.setCircleColor(Color.parseColor("#2196F3"));
+        set.setCircleColorHole(Color.parseColor("#2196F3"));
+        set.setFillColor(Color.parseColor("#2196F3"));
+        set.setHighLightColor(Color.parseColor("#2196F3"));
+        set.setValueTextColor(Color.parseColor("#ECEFF1"));
         set.setValueTextSize(0f);
 
         return set;
     }
+
+  /*  private LineDataSet plasmaDataSet() {
+
+        LineDataSet set = new LineDataSet(null, "Plasma");
+        set.setColor(Color.parseColor("#D50000"));
+        set.setLineWidth(1f);
+        set.setCircleSize(1f);
+        set.setCircleColor(Color.parseColor("#D50000"));
+        set.setCircleColorHole(Color.parseColor("#D50000"));
+        set.setFillColor(Color.parseColor("#D50000"));
+        set.setHighLightColor(Color.parseColor("#D50000"));
+        set.setValueTextColor(Color.parseColor("#212121"));
+        set.setValueTextSize(0f);
+
+        return set;
+    }
+
+    private LineDataSet gasDataSet() {
+
+        LineDataSet set = new LineDataSet(null, "Gas");
+        set.setColor(Color.parseColor("#1B5E20"));
+        set.setLineWidth(1f);
+        set.setCircleSize(1f);
+        set.setCircleColor(Color.parseColor("#1B5E20"));
+        set.setCircleColorHole(Color.parseColor("#1B5E20"));
+        set.setFillColor(Color.parseColor("#1B5E20"));
+        set.setHighLightColor(Color.parseColor("#1B5E20"));
+        set.setValueTextColor(Color.parseColor("#212121"));
+        set.setValueTextSize(0f);
+
+        return set;
+    }*/
 
     public void Click(View view) {
      switch (view.getId()){
@@ -235,11 +288,131 @@ public class ProgressPart extends AppCompatActivity  {
         Intent intent=new Intent(ProgressPart.this,GirisEkrani.class);
         startActivity(intent);
     }
+
     public static void setVacuumNew(int vacuumNew) {
         VacuumNew = vacuumNew;
     }
 
     static int VacuumNew;
+
+    public void drawVacuumGraph (){
+
+        //Grafiğin kurulması
+        mChart=(LineChart) findViewById(R.id.graphVacuum);
+        mChart.setDescription("");
+        mChart.setNoDataText("No data");
+        mChart.setHighlightEnabled(true);
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.parseColor("#E3F2FD"));
+
+        LineData data=new LineData();
+        data.setValueTextColor(Color.parseColor("#0D47A1"));
+        mChart.setData(data);
+
+        Legend l =mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.parseColor("#0D47A1"));
+
+        XAxis Xl=mChart.getXAxis();
+        Xl.setTextColor(Color.parseColor("#0D47A1"));
+        Xl.setDrawGridLines(false);
+        Xl.setAvoidFirstLastClipping(false);
+
+        YAxis Yl=mChart.getAxisLeft();
+        Yl.setDrawGridLines(false);
+        Yl.setTextColor(Color.parseColor("#0D47A1"));
+        Yl.setStartAtZero(false);
+        Yl.setAxisMaxValue(0f);
+        Yl.setAxisMinValue(-100f);
+        YAxis Yaxis=mChart.getAxisRight();
+        Yaxis.setEnabled(false);
+
+
+    }
+/*
+
+    public void drawPlasmaGraph (){
+
+        //Grafiğin kurulması
+        mChart=(LineChart) findViewById(R.id.graphPlasma);
+        mChart.setDescription("");
+        mChart.setNoDataText("No data");
+        mChart.setHighlightEnabled(true);
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.parseColor("#F9FBE7"));
+
+        LineData data=new LineData();
+        data.setValueTextColor(Color.parseColor("#006064"));
+        mChart.setData(data);
+
+        Legend l =mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.parseColor("#006064"));
+
+        XAxis Xl=mChart.getXAxis();
+        Xl.setTextColor(Color.parseColor("#006064"));
+        Xl.setDrawGridLines(false);
+        Xl.setAvoidFirstLastClipping(false);
+
+        YAxis Yl=mChart.getAxisLeft();
+        Yl.setDrawGridLines(false);
+        Yl.setTextColor(Color.parseColor("#006064"));
+        Yl.setStartAtZero(false);
+        Yl.setAxisMaxValue(0f);
+        Yl.setAxisMinValue(-100f);
+        YAxis Yaxis=mChart.getAxisRight();
+        Yaxis.setEnabled(false);
+
+
+    }
+
+    public void drawGasGraph (){
+
+        //Grafiğin kurulması
+        mChart=(LineChart) findViewById(R.id.graphGas);
+        mChart.setDescription("");
+        mChart.setNoDataText("No data");
+        mChart.setHighlightEnabled(true);
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.parseColor("#F9FBE7"));
+
+        LineData data=new LineData();
+        data.setValueTextColor(Color.parseColor("#006064"));
+        mChart.setData(data);
+
+        Legend l =mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.parseColor("#006064"));
+
+        XAxis Xl=mChart.getXAxis();
+        Xl.setTextColor(Color.parseColor("#006064"));
+        Xl.setDrawGridLines(false);
+        Xl.setAvoidFirstLastClipping(false);
+
+        YAxis Yl=mChart.getAxisLeft();
+        Yl.setDrawGridLines(false);
+        Yl.setTextColor(Color.parseColor("#006064"));
+        Yl.setStartAtZero(false);
+        Yl.setAxisMaxValue(0f);
+        Yl.setAxisMinValue(-100f);
+        YAxis Yaxis=mChart.getAxisRight();
+        Yaxis.setEnabled(false);
+
+
+    }
+*/
 
 
 
