d5666be8119801c1096e54aabdba4758e234b268
eliftultay <eliftultay@gmail.com>
2017-09-14 15:48:35 +0300

    Time View added, Some of UI parts changed

--- app/src/main/java/com/example/rumeysal/productinformation/ProgressPart.java
+++ app/src/main/java/com/example/rumeysal/productinformation/ProgressPart.java
@@ -53,10 +53,12 @@ public class ProgressPart extends AppCompatActivity  {
         minute=minuteProgress;
         bt=(BTConnect.getBt());
         değer=(TextView) findViewById(R.id.Values) ;
-        value=0;           //TODO: Tüpün içindeki değer her zaman sabitse bu değeri ona göre kur
+        value=0;
+
+        //TODO: Tüpün içindeki değer her zaman sabitse bu değeri ona göre kur
 
        //Grafiğin kurulması
-        mChart=(LineChart) findViewById(R.id.graph);
+        mChart=(LineChart) findViewById(R.id.graphVacuum);
         mChart.setDescription("");
         mChart.setNoDataText("No data");
         mChart.setHighlightEnabled(true);
@@ -65,24 +67,24 @@ public class ProgressPart extends AppCompatActivity  {
         mChart.setScaleEnabled(true);
         mChart.setDrawGridBackground(false);
         mChart.setPinchZoom(true);
-        mChart.setBackgroundColor(Color.BLACK);
+        mChart.setBackgroundColor(Color.parseColor("#F9FBE7"));
 
         LineData data=new LineData();
-        data.setValueTextColor(Color.WHITE);
+        data.setValueTextColor(Color.parseColor("#006064"));
         mChart.setData(data);
 
-        Legend l=mChart.getLegend();
+        Legend l =mChart.getLegend();
         l.setForm(Legend.LegendForm.LINE);
-        l.setTextColor(Color.WHITE);
+        l.setTextColor(Color.parseColor("#006064"));
 
         XAxis Xl=mChart.getXAxis();
-        Xl.setTextColor(Color.WHITE);
+        Xl.setTextColor(Color.parseColor("#006064"));
         Xl.setDrawGridLines(false);
         Xl.setAvoidFirstLastClipping(false);
 
         YAxis Yl=mChart.getAxisLeft();
         Yl.setDrawGridLines(false);
-        Yl.setTextColor(Color.WHITE);
+        Yl.setTextColor(Color.parseColor("#006064"));
         Yl.setStartAtZero(false);
         Yl.setAxisMaxValue(0f);
         Yl.setAxisMinValue(-100f);
@@ -91,14 +93,17 @@ public class ProgressPart extends AppCompatActivity  {
 
 
         //Bluetooth üzerinden veri almak için
+
         bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
             public void onDataReceived(byte[] data, String message) {
                 Log.d("Message",message);
-               String[]veri= message.split(":");  //TODO: Eğer vacuum değeri alınıyorsa, alınan verinin formu  (BT: "vacuum değeri" ) şeklinde olmalıdır. iki nokta ve BT kısmı önemli
+               String[]veri= message.split(":");
+                //TODO: Eğer vacuum değeri alınıyorsa, alınan verinin formu  (BT: "vacuum değeri" ) şeklinde olmalıdır. iki nokta ve BT kısmı önemli
                 if(veri[0].equals("BT")){
                         value=Float.valueOf(veri[1].trim());        //alınan veri 'value' ya kurulup sonra 'value' plot ediliyor
 
-                   }
+                }
+
                 if((message.trim()).equals("finish")){     //TODO: Bittiğini anlamak için 'finish' komutu gönderilmeli
                     bt.stopService();                      //veri alımı bittiği için bluetooth servisi duruyor
                     timer.setProgress(100);
@@ -107,15 +112,7 @@ public class ProgressPart extends AppCompatActivity  {
 
 
                 }
-
-
-
-
         }});
-
-
-
-
     }
 
     @Override
@@ -128,12 +125,10 @@ public class ProgressPart extends AppCompatActivity  {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if(value!=checkValue){          //Grafiğe yeni bir değerin girdiğini farketmesi için yapıldı. Diğer türlü aynı veriden 3 kere giriliyordu
-                                 addEntry();            //Grafiğe veriyi yazdırmak için
+                            if(value!=checkValue){  //Grafiğe yeni bir değerin girdiğini farketmesi için yapıldı. Diğer türlü aynı veriden 3 kere giriliyordu
+                                 addEntry();        //Grafiğe veriyi yazdırmak için
                                  değer.setText(String.valueOf(value));           //ekranda yazılan veriyi göstermek için
                                  value=checkValue;
-
-
                             }
 
                         }
@@ -143,7 +138,7 @@ public class ProgressPart extends AppCompatActivity  {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }while (value!=(VacuumNew+1));          //Ayarlanan vakum değerinde grafiğin yazımının bitmesi için //TODO: eğer orda yazandan daha farklı bir değerde çalışma ihtimali varsa cihazdan alınan veriye göre düzenlenmesi lazım
+                }while (value!=(VacuumNew+1)); //Ayarlanan vakum değerinde grafiğin yazımının bitmesi için //TODO: eğer orda yazandan daha farklı bir değerde çalışma ihtimali varsa cihazdan alınan veriye göre düzenlenmesi lazım
 
             }
         }).start();
@@ -156,11 +151,8 @@ public class ProgressPart extends AppCompatActivity  {
         bt.stopService();       //uygulama kapandığında bluetooth u kapatmak için
     }
 
-
-
-
     public void addEntry(){
-        LineData data=mChart.getLineData();
+        LineData data = mChart.getLineData();
 
         if(data!=null){
             LineDataSet set =data.getDataSetByIndex(0);
@@ -171,7 +163,7 @@ public class ProgressPart extends AppCompatActivity  {
 
             }
             data.addXValue("");
-            data.addEntry(new Entry(value,set.getEntryCount()),0);          //yeni eklenen verinin grafiğe eklenmesi için
+            data.addEntry(new Entry(value,set.getEntryCount()),0); //yeni eklenen verinin grafiğe eklenmesi için
             mChart.notifyDataSetChanged();
             mChart.setVisibleXRange(200);           //Bütün verilerin aynı grafikte görünmesi için. Eğer veri alındıkça kayması isteniyorsa burdaki sayının  küçüklmesi gerek. Mesela 10'a filan kurulablir.
             mChart.moveViewToX(data.getXValCount());
@@ -228,7 +220,6 @@ public class ProgressPart extends AppCompatActivity  {
                     })
                     .setNegativeButton("View Screen",null);             //Grafiği görmek için
             dialog.show();
-
         }
 
     }
