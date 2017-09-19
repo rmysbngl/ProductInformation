63d7cc4bc2544ebefd225d3886e8c861fdf0c1a0
eliftultay <eliftultay@gmail.com>
2017-09-19 09:53:32 +0300

    Created Sign In Layout

---
+++ app/src/main/java/com/example/rumeysal/productinformation/SignIn.java
@@ -0,0 +1,30 @@
+package com.example.rumeysal.productinformation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class SignIn extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+    }
+    
+
+}
